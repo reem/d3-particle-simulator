880b15ac2fd01bd948f3f7ac49c0d03c7d7e02c4
Jonathan Reem <jonathan.reem@gmail.com>
2014-04-26 18:22:01 -0700

    Documented init.

--- init.js
+++ init.js
@@ -1,35 +1,45 @@
+// Global constants, can be set by sliders.
 var gravityStrength = 0.2;
 var chargeStrength = 0.4;
 var chargeFunction = function (d, i) {
+  //                              the mouse charge value ->  ****
   return i ? (Math.random() - 0.8) * chargeStrength * 100 : -4000;
 };
 
+// Create the global svg element.
 var svg = d3.select("#main").append("svg:svg")
   .attr("width", window.innerWidth)
   .attr("height", window.innerHeight);
 
 $(document).ready(function () {
+  // Create our wonderful sliders using dragdealer.js
   var gravitySlider = new Dragdealer("gravity", {x: gravityStrength});
   var chargeSlider = new Dragdealer("charge", {x: chargeStrength});
   var particleSlider = new Dragdealer("particles", {x: numParticles});
 
+  // Create our particles and root.
   updateParticles();
   updateRoot();
 
+  // Register the collision detection to be checked
+  // on every tick.
   onTick(function () {
     var particles = getParticles();
     var quadTree = d3.geom.quadtree(particles);
 
     var n = getParticles().length;
     for (var i = 1; i < n; i++) {
+      // Map our collision function over the three.
       quadTree.visit(collide(particles[i]));
     }
 
+    // Update the position of all circles.
     svg.selectAll("circle")
       .attr("cx", function (d) { return d.x; })
       .attr("cy", function (d) { return d.y; });
   });
 
+  // Move the root node around whenever we move the mouse.
   svg.on("mousemove", function () {
     var p1 = d3.mouse(this);
     root.px = p1[0];
@@ -37,15 +47,22 @@ $(document).ready(function () {
     resumeForce();
   });
 
+  // Our global event loop.
+  // Could be refactored to use window.requestAnimationFrame.
   setInterval(function () {
+    // Set the strength of gravity and charge by getting them from
+    // the slider.
     gravityStrength = gravitySlider.getValue()[0] * 2.5 + 0.00001;
     chargeStrength = chargeSlider.getValue()[0] * 10;
 
-    var oldParticles = numParticles;
+    // Set the number of particles and update them.
     numParticles = particleSlider.getValue()[0] * 2000;
+    newParticles();
 
-    newParticles(oldParticles, numParticles);
+    // Update our force with the new values.
     updateForce(getParticles(), gravityStrength, chargeFunction);
+
+    // Reset the force's alpha.
     resetForce();
   }, 100);
 });
